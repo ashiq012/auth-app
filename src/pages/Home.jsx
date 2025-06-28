505dce57f7074e595522c89ff8417b81fe4aed25
Md Ashiq ilahi <128233331+ashiq012@users.noreply.github.com>
2025-06-28 14:22:42 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
   // Protected Route
   const Protected = async () => {
     try {
-      const url = 'http://localhost:4000/api/v1/protected'
+      const url = 'https://auth-backend-gray.vercel.app/api/v1/protected'
       const headers = {
         headers : {
           'Authorization' : localStorage.getItem('token')
